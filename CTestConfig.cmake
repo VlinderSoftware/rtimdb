e34c889d6af1e26f55e0fd0a22ffb121c0307808
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2015-07-08 20:47:01 -0400

    add CTestConfig.cmake

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "rtimdb")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "192.168.200.81")
+set(CTEST_DROP_LOCATION "/submit.php?project=rtimdb")
+set(CTEST_DROP_SITE_CDASH TRUE)
