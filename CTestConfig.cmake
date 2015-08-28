e9593195c06d711ef1dc4fb8f0a1b0b50f595b6b
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2015-08-27 20:56:20 -0400

    Update CDash configuration

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -2,6 +2,6 @@ set(CTEST_PROJECT_NAME "rtimdb")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "192.168.200.81")
-set(CTEST_DROP_LOCATION "/submit.php?project=rtimdb")
+set(CTEST_DROP_SITE "192.168.200.87")
+set(CTEST_DROP_LOCATION "/cdash/submit.php?project=rtimdb")
 set(CTEST_DROP_SITE_CDASH TRUE)
